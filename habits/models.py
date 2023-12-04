52d9a748337c07ff5659bfcd4f8594702b8976b5
Nikkon97 <you@example.com>
2023-12-04 20:53:27 +0300

    creating a model

--- habits/models.py
+++ habits/models.py
@@ -3,17 +3,17 @@ from django.db import models
 import users.models
 
 
-class Habits(models.Model):
+class Habit(models.Model):
     place = models.CharField(max_length=50, verbose_name='место')
     time = models.TimeField
     action = models.CharField(max_length=100, verbose_name='действие')
     is_pleasant = models.BooleanField
-    linked = models.ForeignKey('self')
+    linked = models.ForeignKey('self', on_delete=models.CASCADE)
     period = models.IntegerField
     reward = models.CharField
     length = models.IntegerField
     is_public = models.BooleanField
-    owner = models.ForeignKey(users.models.User)
+    owner = models.ForeignKey(users.models.User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.title}'
